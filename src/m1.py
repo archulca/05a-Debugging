de689123d762986596af573d1a74839a72c7aeef
Carla Archuleta <archulca@rose-hulman.edu>
2019-02-04 15:16:54 -0500

    TODO #9

--- src/m1.py
+++ src/m1.py
@@ -206,7 +206,7 @@ def broken_2(x, y, window):
 
 
 # -----------------------------------------------------------------------------
-# TODO: 5. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 5. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # -----------------------------------------------------------------------------
 def broken_3(n, point, length, distance_between_lines, window):
@@ -315,7 +315,7 @@ def broken_6(n):
 
 
 # -----------------------------------------------------------------------------
-# TODO: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # -----------------------------------------------------------------------------
 def broken_7(n, point, length, distance_between_lines, window):
@@ -348,6 +348,7 @@ def broken_7(n, point, length, distance_between_lines, window):
         line = rg.Line(left, right)
         line.attach_to(window)
         window.render(0.5)
+
         left = rg.Point(left.x, left.y + distance_between_lines)
         right = rg.Point(right.x, right.y + distance_between_lines)
     window.close_on_mouse_click()
